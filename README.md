b8ba8a36919eec4d578bd7b84ed1be2a78469f40
Roopam Poddar <mailroopam@gmail.com>
2018-04-25 00:11:52 +0530

    pep8 compatibility changes

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This repo contains a python script to extract three kinds of data from a
 postgresql news database.
 
 1. What are the most popular three articles of all time.
-2. What are the most popular arcticle authors of all time.
+2. What are the most popular article authors of all time.
 3. On which days did more than 1% of requests lead to errors.
 
 ## How to Run
@@ -13,4 +13,16 @@ postgresql news database.
 * Make sure that postgresql server is up and running with the news database imported
 * Run the script by `python3 generate_report.py`
 
-This will create a report.txt file which contains human readable data for the above three queries.
+This will create a report.txt file which contains human readable data for the above three queries in the same diretory.
+
+## Design
+
+The script use `psycopg2` module to connect to the database and run queries.
+All the three data sets have been done using a single query using joins, the result of which
+are stored in three seprate variables which is later used to write into a file to create the report.
+
+All three queries use subqueries to create a data set, on which join is performed. 
+We also using string operations `||` to concatenate strings to match article slugs with path in 
+log table, since it contains the full URI including `/article/`
+
+We are also using `cast()` in the third query to find out percentage upto 2 decimal places.  
--- generate_report.py
+++ generate_report.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import psycopg2
 
 db = psycopg2.connect(database='news')
@@ -5,42 +7,42 @@ c = db.cursor()
 
 # First Query
 c.execute("""
-    select 
-        articles.title, 
-        l.count 
+    select
+        articles.title,
+        l.count
     from
         (
-            select 
-                path, 
-                count(*) as count 
-            from log 
-            where 
-                log.path != '/' 
-            group by path 
-            order by count(*) desc 
+            select
+                path,
+                count(*) as count
+            from log
+            where
+                log.path != '/'
+            group by path
+            order by count(*) desc
             limit 3
-        ) l join articles 
-    on l.path = '/article/' || articles.slug 
-    order by l.count desc 
+        ) l join articles
+    on l.path = '/article/' || articles.slug
+    order by l.count desc
     """)
 top_articles = c.fetchall()
 
 # Top authors
 c.execute("""
-    select 
-        authors.name, 
-        l.count 
+    select
+        authors.name,
+        l.count
     from (
-        select 
-            count(*), 
-            articles.author 
-        from log, articles 
-        where 
-            log.path = '/article/' || articles.slug and 
-            log.path != '/' 
-        group by articles.author 
+        select
+            count(*),
+            articles.author
+        from log, articles
+        where
+            log.path = '/article/' || articles.slug and
+            log.path != '/'
+        group by articles.author
         order by count(*) desc
-    ) l join authors 
+    ) l join authors
     on authors.id = l.author
     order by l.count desc
 """)
@@ -48,28 +50,28 @@ top_authors = c.fetchall()
 
 # Days with more than 1% errors
 c.execute("""
-    select 
-        error_table.date, 
+    select
+        error_table.date,
         round(
-            (cast(error_table.count*100 as decimal)/all_table.count), 2) || 
+            (cast(error_table.count*100 as decimal)/all_table.count), 2) ||
             '%'
-        from 
-        ( 
-            select 
-                date(time) as date, 
-                count(*)  as count 
-            from log 
-            where status != '200 OK' 
-            group by date(time) 
+        from
+        (
+            select
+                date(time) as date,
+                count(*)  as count
+            from log
+            where status != '200 OK'
+            group by date(time)
         ) error_table join
-        ( 
-            select 
-                date(time) as date, 
-                count(*)  as count 
-            from log 
-            group by date(time) 
-        ) all_table 
-        on error_table.date = all_table.date 
+        (
+            select
+                date(time) as date,
+                count(*)  as count
+            from log
+            group by date(time)
+        ) all_table
+        on error_table.date = all_table.date
         where error_table.count * 100 > all_table.count
 """)
 error_days = c.fetchall()
@@ -95,5 +97,3 @@ for row in error_days:
 file.close()
 
 db.close()
-
-
